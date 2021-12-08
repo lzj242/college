06e515539f99d0610da98f757c02bfb183c8d984
feng <2128258195@qq.com>
2021-12-08 15:40:49 +0800

    first

---
+++ c++/Project2/Project2/源.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 7, m = 8;
+	vector<int>nums(n);
+	bool q=true;
+	cout << q<<"\n";
+	cout << nums.size() << "\n";
+	int a = nums[4];
+	nums[0] = 11;
+	nums.push_back(20);
+	cout << nums.size() << "\n";
+	int b = nums.back();
+	cout << b << "\n";
+	nums.pop_back();
+	cout << nums.size() << "\n";
+	swap(nums[0], nums[1]);
+	return 0;
+}
